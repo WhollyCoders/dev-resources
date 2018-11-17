c97803e253112e79bc77c2fff1709d6154d8f15e
Michael L Parks <github@wmi-inc.com>
2018-11-17 11:50:45 -0500

    Initial Commit

---
+++ data/DaysOfWeek.js
@@ -0,0 +1,32 @@
+const DaysOfWeek = [
+    {
+        name:"sunday",
+        abbr:"sun"
+    },
+    {
+        name:"monday",
+        abbr:"mon"
+    },
+    {
+        name:"tuesday",
+        abbr:"tue"
+    },
+    {
+        name:"wednesday",
+        abbr:"wed"
+    },
+    {
+        name:"thursday",
+        abbr:"thu"
+    },
+    {
+        name:"friday",
+        abbr:"fri"
+    },
+    {
+        name:"saturday",
+        abbr:"sat"
+    }
+];
+
+export default DaysOfWeek;
---
+++ data/Months.js
@@ -0,0 +1,64 @@
+const Months = [
+    {
+        name:"january",
+        abbr:"jan",
+        daysInMonth: 31
+    },
+    {
+        name:"february",
+        abbr:"feb",
+        daysInMonth: 28
+    },
+    {
+        name:"march",
+        abbr:"mar",
+        daysInMonth: 31
+    },
+    {
+        name:"april",
+        abbr:"apr",
+        daysInMonth: 30
+    },
+    {
+        name:"may",
+        abbr:"may",
+        daysInMonth: 31
+    },
+    {
+        name:"june",
+        abbr:"jun",
+        daysInMonth: 30
+    },
+    {
+        name:"july",
+        abbr:"jul",
+        daysInMonth: 31
+    },
+    {
+        name:"august",
+        abbr:"aug",
+        daysInMonth: 31
+    },
+    {
+        name:"september",
+        abbr:"sep",
+        daysInMonth: 30
+    },
+    {
+        name:"october",
+        abbr:"oct",
+        daysInMonth: 31
+    },
+    {
+        name:"november",
+        abbr:"nov",
+        daysInMonth: 30
+    },
+    {
+        name:"december",
+        abbr:"dec",
+        daysInMonth: 31
+    }
+];
+
+export default Months;
